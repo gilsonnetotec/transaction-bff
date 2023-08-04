c2413f10371f507fd03895c7e2e9e9c22b30559e
tec <gilsonnetotec@gmail.com>
2023-08-04 11:46:26 -0300

    init

---
+++ src/test/java/br/com/gilsontec/transactionbff/TransactionBffApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gilsontec.transactionbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
